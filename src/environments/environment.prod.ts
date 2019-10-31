4f66f5ec66c96302051ab3c652d879adf7076e55
Антон Забелин <a.zabelin@z-it.ru>
2019-10-31 11:46:10 +0300

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: "AIzaSyDStV5zkZtOUfItc5dSQv4ekHhW67pH6Ow"
 };
---
+++ src/environments/interface.ts
@@ -0,0 +1,15 @@
+
+export interface Environment {
+    production: boolean;
+    apiKey: string;
+}
+
+/*
+apiKey: string = "AIzaSyDStV5zkZtOUfItc5dSQv4ekHhW67pH6Ow";
+authDomain: string = "angular-blog-e9bca.firebaseapp.com";
+databaseURL: string = "https://angular-blog-e9bca.firebaseio.com";
+projectId: string = "angular-blog-e9bca";
+storageBucket: string = "angular-blog-e9bca.appspot.com";
+messagingSenderId: string = "491957225546";
+appId: string = "1:491957225546:web:2f9345f2eb8b7227e6feb3";
+*/
